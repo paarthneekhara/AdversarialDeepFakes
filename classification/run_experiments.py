6903be1ad15e13040d6f5b9ab04c82fb59b4ab4f
Shehzeen Hussain <shehzeensh@Shehzeens-MacBook-Pro.local>
2019-11-20 01:01:32 -0800

    added comments and cleaned code

--- classification/run_experiments.py
+++ classification/run_experiments.py
@@ -1,19 +1,29 @@
-
+"""
+Author: Shehzeen Hussain
+"""
 
 import os
 import argparse
 from os.path import join
 import numpy
 
-if __name__ == '__main__':
+def main():
+    """
+    This file automates the running of various experiments 
+    for different attacks to generate adversarial videos and
+    detections on the newly generated adversarial video using specified detector model.
+    Keeps track of results of attacks on different detector models
+    and the dataset used for attack.
+
+    """
     p = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    p.add_argument('--model_dir', '-mdir', type=str, default=None)
-    p.add_argument('--model_type', '-mtype', type=str, default="c23")
-    p.add_argument('--data_dir', '-data', type=str, default=None)
-    p.add_argument('--exp_folder', '-exp', type=str, default=None)
-    p.add_argument('--faketype', type=str, default=None)
+    p.add_argument('--model_dir', '-mdir', type=str, default=None) #dir contains all_c23.p etc
+    p.add_argument('--model_type', '-mtype', type=str, default="c23") #c23, c40 or raw
+    p.add_argument('--data_dir', '-data', type=str, default=None) # dir containing face2face etc
+    p.add_argument('--exp_folder', '-exp', type=str, default=None) # where sub directories will be created
+    p.add_argument('--faketype', type=str, default=None) # face2face, neural textures etc
     p.add_argument('--attack', '-a', type=str, default="iterative_fgsm")
     p.add_argument('--compress', action='store_true')
     p.add_argument('--cuda', action='store_true')
@@ -60,21 +70,10 @@ if __name__ == '__main__':
     if cuda_run:
         string_command_detect += " --cuda"
 
-    
+    os.system(string_command_attack)
+    os.system(string_command_detect)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
 
