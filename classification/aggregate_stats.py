f16a580d56395284e2d369b859d91810cb3dbbd8
Paarth Neekhara <paarth.n@gmail.com>
2019-11-20 22:17:15 -0800

    continuous eval

--- classification/aggregate_stats.py
+++ classification/aggregate_stats.py
@@ -1,13 +1,10 @@
-import os, sys
-import argparse
+import os
 from os.path import join
-import numpy
-import attack
-import detect_from_video
-from tqdm import tqdm
+import argparse
 import copy
 import json
 import pprint
+import time
 
 def main():
     """
@@ -29,65 +26,68 @@ def main():
 
     experiment_stats = {}
 
-    for fake_type in fake_types:
-        experiment_stats[fake_type] = {}
-        for compression_type in compression_types:
-            experiment_stats[fake_type][compression_type] = {}
-            for attack_method in attack_methods:
-                
-                for extension in ["", "_compressed"]:
-                    experiment_stats[fake_type][compression_type]["{}{}".format(attack_method, extension)] = {}
-                    adv_videos_dir = join(experiment_dir, fake_type, compression_type, "adv_{}{}".format(attack_method, extension))
-                    detected_videos_dir = join(experiment_dir, fake_type, compression_type, "adv_{}_detected{}".format(attack_method, extension))
+    while True:
+        # sleep for minute
+        time.sleep(10)
+        for fake_type in fake_types:
+            experiment_stats[fake_type] = {}
+            for compression_type in compression_types:
+                experiment_stats[fake_type][compression_type] = {}
+                for attack_method in attack_methods:
                     
-                    if os.path.isdir(adv_videos_dir) and os.path.isdir(detected_videos_dir):
-                        attack_json_files = os.listdir(adv_videos_dir)
-                        attack_json_files = [file for file in attack_json_files if file.endswith("metrics_attack.json")]
+                    for extension in ["", "_compressed"]:
+                        experiment_stats[fake_type][compression_type]["{}{}".format(attack_method, extension)] = {}
+                        adv_videos_dir = join(experiment_dir, fake_type, compression_type, "adv_{}{}".format(attack_method, extension))
+                        detected_videos_dir = join(experiment_dir, fake_type, compression_type, "adv_{}_detected{}".format(attack_method, extension))
+                        
+                        if os.path.isdir(adv_videos_dir) and os.path.isdir(detected_videos_dir):
+                            attack_json_files = os.listdir(adv_videos_dir)
+                            attack_json_files = [file for file in attack_json_files if file.endswith("metrics_attack.json")]
 
-                        detect_json_files = os.listdir(detected_videos_dir)
-                        detect_json_files = [file for file in detect_json_files if file.endswith("metrics.json")]
+                            detect_json_files = os.listdir(detected_videos_dir)
+                            detect_json_files = [file for file in detect_json_files if file.endswith("metrics.json")]
 
-                        attack_metrics = {
-                            'avg_frame_fooling_rate' : None,
-                            'avg_video_fooling_rate' : None,
-                            'total_videos_fooled' : 0,
-                            'total_frames_fooled' : 0,
-                            'avg_linf_norm' : None,
-                            'total_videos' : 0,
-                            'total_frames' : 0
-                        }
-
-
-                        for detect_file in detect_json_files:
-                            with open(join(detected_videos_dir, detect_file)) as f:
-                                _attack_metrics = json.loads(f.read())
-                                attack_metrics['total_frames'] += _attack_metrics['total_frames']
-
-                                if _attack_metrics['percent_fake_frames'] < 0.5:
-                                    attack_metrics['total_videos_fooled'] += 1.
-
-                                attack_metrics['total_videos'] += 1.
-                                attack_metrics['total_frames_fooled'] += _attack_metrics['total_real_frames']
-
-                        if attack_metrics['total_videos'] > 0 and attack_metrics['total_frames'] > 0:
-                            attack_metrics['avg_video_fooling_rate'] = attack_metrics['total_videos_fooled']/attack_metrics['total_videos']
-                            attack_metrics['avg_frame_fooling_rate'] = attack_metrics['total_frames_fooled']/attack_metrics['total_frames']
-
-                        total_l_infnorm = 0.0
-                        total_frames = 0.0
-                        for attack_file in attack_json_files:
-                            with open(join(adv_videos_dir, attack_file)) as f:
-                                _attack_metrics = json.loads(f.read())
-                                for frame_meta_data in _attack_metrics['attack_meta_data']:
-                                    total_l_infnorm += frame_meta_data['l_inf_norm']
-                                    total_frames += 1.
-
-                        attack_metrics['avg_linf_norm'] = total_l_infnorm/total_frames
+                            attack_metrics = {
+                                'avg_frame_fooling_rate' : None,
+                                'avg_video_fooling_rate' : None,
+                                'total_videos_fooled' : 0,
+                                'total_frames_fooled' : 0,
+                                'avg_linf_norm' : None,
+                                'total_videos' : 0,
+                                'total_frames' : 0
+                            }
 
 
-                        experiment_stats[fake_type][compression_type]["{}{}".format(attack_method, extension)] = copy.copy(attack_metrics)
-                        with open(join(experiment_dir, "agg_exp_stats.json"), "w") as wf:
-                            wf.write(json.dumps(experiment_stats))
+                            for detect_file in detect_json_files:
+                                with open(join(detected_videos_dir, detect_file)) as f:
+                                    _attack_metrics = json.loads(f.read())
+                                    attack_metrics['total_frames'] += _attack_metrics['total_frames']
+
+                                    if _attack_metrics['percent_fake_frames'] < 0.5:
+                                        attack_metrics['total_videos_fooled'] += 1.
+
+                                    attack_metrics['total_videos'] += 1.
+                                    attack_metrics['total_frames_fooled'] += _attack_metrics['total_real_frames']
+
+                            if attack_metrics['total_videos'] > 0 and attack_metrics['total_frames'] > 0:
+                                attack_metrics['avg_video_fooling_rate'] = attack_metrics['total_videos_fooled']/attack_metrics['total_videos']
+                                attack_metrics['avg_frame_fooling_rate'] = attack_metrics['total_frames_fooled']/attack_metrics['total_frames']
+
+                            total_l_infnorm = 0.0
+                            total_frames = 0.0
+                            for attack_file in attack_json_files:
+                                with open(join(adv_videos_dir, attack_file)) as f:
+                                    _attack_metrics = json.loads(f.read())
+                                    for frame_meta_data in _attack_metrics['attack_meta_data']:
+                                        total_l_infnorm += frame_meta_data['l_inf_norm']
+                                        total_frames += 1.
+
+                            attack_metrics['avg_linf_norm'] = total_l_infnorm/total_frames
+
+
+                            experiment_stats[fake_type][compression_type]["{}{}".format(attack_method, extension)] = copy.copy(attack_metrics)
+                            with open(join(experiment_dir, "agg_exp_stats.json"), "w") as wf:
+                                wf.write(json.dumps(experiment_stats))
 
     pprint.pprint(experiment_stats)
 
